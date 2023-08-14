d6891e6d5c5cbfec17f4e55eb79eca88448be5fb
Fermin Aguilar <ferminab99@gmail.com>
2023-08-14 19:54:30 -0300

    se corrigieron errores

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
-import CountDown from './components/CountDown';
+
 import Descanso from './components/Descanso';
 import InfoCeremonias from './components/InfoCeremonias';
-import InfoIglesia from './components/InfoIglesia';
+
 import Portada from './components/Portada.js';
 
 function App() {
