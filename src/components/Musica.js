0d2d27ecaccce53466b37da0170e43fe059c4615
Fermin Aguilar <ferminab99@gmail.com>
2023-08-26 11:25:06 -0300

    se agrego el link de spotify

--- src/components/Musica.js
+++ src/components/Musica.js
@@ -10,7 +10,7 @@ const Musica = () => {
         <div className="infoMusica">
             <h5>¿Que canción no puede faltar?</h5>
           <p>¡Queremos crear una playlist inolvidable!</p>
-          <button><a href="https://open.spotify.com/intl-es">Sugerir canción</a></button>
+          <button><a href="https://open.spotify.com/playlist/5fKNFOfnVcTy1S7MGwwE9B?si=pkI6-MybRhKMQqTgYQMXHQ&pt=f62dab63621addd5591ab45ff4be5400">Sugerir canción</a></button>
         </div>
         </section>
         <div className="dressCode">
